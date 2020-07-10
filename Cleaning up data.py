f275150be5d10606f49d12a6659bb4327a71afae
Artem <Artemness@gmail.com>
2020-07-10 10:36:46 -0400

    Did some rounding work on data.

---
+++ Cleaning up data.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('winequality-red.csv')
+
+#Rounding Chlorides and Density to 3 and 4 decimal places respectfully:
+df['chlorides'] = df['chlorides'].apply(lambda x: round(x,3))
+df['density'] = df['density'].apply(lambda x: round(x,4))
+
+#Checking Data to see for missing values:
+print(df.info())
+
+df.to_csv('wineclean.csv')
