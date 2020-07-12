67e762222d4ef7b7db330c87a3e50798453a0751
Artem <Artemness@gmail.com>
2020-07-11 21:53:44 -0400

    Editted Random Forest Matrix

--- Django/redwineWebApp/redwineWebApp/settings.py
+++ Django/redwineWebApp/redwineWebApp/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from secret import SECRET
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,7 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET
+secretdir = os.path.join(BASE_DIR, 'redwineWebApp', 'secret.txt')
+print(secretdir)
+with open(secretdir) as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
